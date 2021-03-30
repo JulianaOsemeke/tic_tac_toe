aca16366d1bd4a1cc30827d9f15ab89ec41d2153
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-03-30 11:22:39 +0100

    Correct rubocop errors

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
-
 require_relative '../lib/board'
 
-RSpec.describe Board do # rubocop:disable Metrics/BlockLength
+RSpec.describe Board do 
   let(:board) { Board.new }
 
   context 'correctly validates inputs:' do
@@ -29,22 +27,22 @@ RSpec.describe Board do # rubocop:disable Metrics/BlockLength
 
   context 'correctly updates state:' do
     it 'is initially numbered from 1 - 9' do
-      expected =  ' 1 | 2 | 3 ',
-                  '---+---+---',
-                  ' 4 | 5 | 6 ',
-                  '---+---+---',
-                  ' 7 | 8 | 9 '
+      expected = ' 1 | 2 | 3 ',
+                 '---+---+---',
+                 ' 4 | 5 | 6 ',
+                 '---+---+---',
+                 ' 7 | 8 | 9 '
 
       expect(board.display).to eq expected
     end
 
     it 'updates on valid input' do
       board.update(5, 'X')
-      expected =  ' 1 | 2 | 3 ',
-                  '---+---+---',
-                  ' 4 | X | 6 ',
-                  '---+---+---',
-                  ' 7 | 8 | 9 '
+      expected = ' 1 | 2 | 3 ',
+                 '---+---+---',
+                 ' 4 | X | 6 ',
+                 '---+---+---',
+                 ' 7 | 8 | 9 '
 
       expect(board.display).to eq expected
     end
